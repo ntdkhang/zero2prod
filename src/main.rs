d587f3c6cc463243186d466a1783064569ae018d
Khang <ntdkhang143@gmail.com>
2025-04-24 22:07:13 +0700

    move to lib

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,7 @@
-use actix_web::{get, web, App, HttpRequest, HttpServer, Responder};
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
-
+    run()
+        .await
 }
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello, {}", &name)
-}
-
-
